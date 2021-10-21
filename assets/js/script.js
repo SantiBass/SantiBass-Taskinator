59fa93e9c58937688936145e737dec1a3fe9e4ea
SantiBass <sibarraryan@gmail.com>
2021-10-21 16:17:43 -0700

    form-submission-done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,24 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var taskToDoEl = document.querySelector("#tasks-to-do");
 
 
 var createTaskHandler = function() { document.createElement("li");
-    var listItemEl =document.createElement("li");
-listItemEl.className ="task-item";
-listItemEl.textContent = "This is a new task.";
+event.preventDefault();
+var taskNameInput = document.querySelector("input[name='task-name']").value;
+var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+
+
+    var listItemEl = document.createElement("li");
+listItemEl.className = "task-item";
+var taskInfoEl = document.createElement("div")
+taskInfoEl.className = "task-info";
+taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+listItemEl.appendChild(taskInfoEl)
+
+
 taskToDoEl.appendChild(listItemEl);
+console.dir(listItemEl);
 };
 
-buttonEl.addEventListener("click" ,  createTaskHandler);
+formEl.addEventListener("submit" ,  createTaskHandler);
